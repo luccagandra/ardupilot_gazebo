38b2638a2353762d67fbd825e72ffceef7f6ff43
Lovro Markovic <lovro.markovic@fer.hr>
2021-01-19 20:49:14 +0100

    Add mavproxy_start.sh helper script

---
+++ scripts/mavproxy_start.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Helper Script for launching MAVProxy with all necessary arguments.
+
+# Exit immediatelly if a command exits with a non-zero status
+set -e
+
+# Executes a command when DEBUG signal is emitted in this script - should be after every line
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+
+# Executes a command when ERR signal is emmitted in this script
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+PORT_OUT1=$1
+PORT_OUT2=$2
+PORT_MASTER=$3
+PORT_SITL=$4
+ID=$5
+STREAMRATE=$6
+ADDITIONAL_ARGS="${@:7:99}"
+
+if [ -z "${PORT_OUT1}" ] ; then
+  PORT_OUT1="14550"
+fi
+
+if [ -z "${PORT_OUT2}" ] ; then
+  PORT_OUT2="14551"
+fi
+
+if [ -z "${PORT_MASTER}" ] ; then
+  PORT_MASTER="5760"
+fi
+
+if [ -z "${PORT_SITL}" ] ; then
+  PORT_OUT2="5501"
+fi
+
+if [ -z "${ID}" ] ; then
+  ID="1"
+fi
+
+if [ -z "${STREAMRATE}" ] ; then
+  STREAMRATE="50"
+fi
+
+# Single vehicle command
+echo "Start single vehicle MAVProxy command"
+echo "$HOME/.local/bin/mavproxy.py --mav10 --streamrate ${STREAMRATE} --target-system ${ID} --out 127.0.0.1:${PORT_OUT1} --out 127.0.0.1:${PORT_OUT2} --master tcp:127.0.0.1:${PORT_MASTER} --sitl 127.0.0.1:${PORT_SITL}"
+$HOME/.local/bin/mavproxy.py --mav10 --streamrate ${STREAMRATE} --target-system ${ID} --out 127.0.0.1:${PORT_OUT1} --out 127.0.0.1:${PORT_OUT2} --master tcp:127.0.0.1:${PORT_MASTER} --sitl 127.0.0.1:${PORT_SITL}
+echo "MAVProxy shutdown"
