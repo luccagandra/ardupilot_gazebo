ceb846c12186ef63cefab4fb6c3fe9950b475803
Lovro Markovic <lovro.markovic@fer.hr>
2021-06-17 14:19:41 +0200

    [CI] Correctly checkout new version

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -19,18 +19,12 @@ git clone https://github.com/lmark1/uav_ros_simulation.git
 cd uav_ros_simulation
 
 echo "running the main install.sh"
-./installation/install.sh
-
-gitman update ardupilot --skip-lock --force
-gitman update
+./installation/install_and_setup_workspace.sh
 
 # checkout the SHA
 cd ~/uav_ros_simulation/.gitman/$PACKAGE_NAME
 git checkout "$SHA"
 
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/src
-ln -s ~/uav_ros_simulation
 source /opt/ros/$ROS_DISTRO/setup.bash
 cd ~/catkin_ws
 
